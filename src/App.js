7e202de5e7406899e5334bd699f75c57195b6433
Basudev Singh <bassingh@bassingh-mn1.linkedin.biz>
2022-08-13 14:46:44 +0530

    Before Firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Cart from './Cart';
+import Navbar from './Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 999,
+          title: 'Watch',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1524805444758-089113d48a6d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=988&q=80',
+          id: 1
+        },
+        {
+          price: 1000,
+          title: 'Mobile Phone',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1601784551446-20c9e07cdbdb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=967&q=80',
+          id: 2
+        },
+        {
+          price: 1099,
+          title: 'Laptop',
+          qty: 1,
+          img: 'https://images.unsplash.com/photo-1525547719571-a2d4ac8945e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1364&q=80',
+          id: 3
+        }
+      ]
+    }
+    //this.increaseQuantity = this.increaseQuantity.bind(this);
+  }
+  handleIncreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products: products
+    })
+  }
+
+  handleDecreaseQuantity = (product) => {
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty == 0) {
+      return;
+    }
+    products[index].qty -= 1;
+
+    this.setState({
+      products: products
+    })
+  }
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id);
+
+    this.setState({
+      products: items
+    })
+  }
+
+  getCartCount() {
+    const { products } = this.state;
+
+    let count = 0;
+
+    products.forEach((product) => {
+      count += product.qty;
+    })
+
+    return count;
+  }
+
+  getCartTotal = () => {
+    const { products } = this.state;
+
+    let cartTotal = 0;
+
+    products.map((product) => {
+      cartTotal = cartTotal + product.qty * product.price;
+    })
+
+    return cartTotal;
+  }
+  render() {
+    const { products } = this.state;
+    return (
+      <div className="App">
+        <Navbar
+        count = {this.getCartCount()} 
+        />
+        <Cart 
+        products = {products}
+        onIncreaseQuantity = {this.handleIncreaseQuantity}
+        onDecreaseQuantity = {this.handleDecreaseQuantity}
+        onDeleteProduct = {this.handleDeleteProduct}
+        />
+        <div style = { {fontSize: 20, padding: 10 }}>TOTAL: {this.getCartTotal()}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
